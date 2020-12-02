57521f45c180b6194bd7e77d092f152684aa6b7c
slootsky <justin@slootsky.org>
2020-12-01 21:30:12 -0500

    ran black

--- adafruit_matrixportal/matrixportal.py
+++ adafruit_matrixportal/matrixportal.py
@@ -201,7 +201,8 @@ class MatrixPortal:
 
         if scrolling and self._scrolling_index is None:  # Not initialized yet
             self._scrolling_index = self._get_next_scrollable_text_index()
-        return len(self._text)-1
+        return len(self._text) - 1
+
     # pylint: enable=too-many-arguments
 
     @staticmethod
